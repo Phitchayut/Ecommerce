9feb8d372f64a2cae54f3adbad7a3f68f4cba3fc
Phitchayut <70578824+Phitchayut@users.noreply.github.com>
2022-09-14 20:32:49 +0700

    install tailwind & mui

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,8 @@ import './App.css'
 function App() {
 
   return (
-    <div className="App">
-      
+    <div className="App flex justify-center">
+          <h1 className='text-center mt-5 font-bold text-3xl text-blue-300'>Hello Ecommerce</h1>
     </div>
   )
 }
